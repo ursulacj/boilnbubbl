4c410d6e4b1d9a7a5843033e574902320646676d
ursulacj <ursulacedillojohnson@gmail.com>
2019-04-06 12:29:25 -0500

    committing more folders and folder moves

--- src/App/App.js
+++ src/App/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>Boil N Bubbl</header>
+        <header className="App-header">Boil N Bubbl</header>
       </div>
     );
   }
