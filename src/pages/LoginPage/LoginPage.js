a40e755140fbba23721a1c4c95a416ba9947ce59
ursulacj <ursulacedillojohnson@gmail.com>
2019-04-08 16:11:10 -0500

    adding login slider effect :

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import NavBar from '../../components/NavBar/NavBar';
 import './LoginPage.css';
 
@@ -8,49 +7,88 @@ class LoginPage extends Component {
     state = {
         username: '',
         email: '',
-        pw: ''
+        pw: '',
+        clicked: false
     };
 
+/*-----------------Event Handlers----------------*/ 
+
+handleSlideEffect = () => {
+    console.log('click event handled!');
+
+    this.state.clicked ? this.setState({clicked: false}) : this.setState({clicked: true}); 
+}
+
     render() {
+
+        const loginContainer =  this.state.clicked  ? 'fullLoginContainer right-panel-active' : 'fullLoginContainer'; 
+
         return(
             <div>
                 <NavBar />
 
-                <div class="container" id="container">
-                    <div class="form-container sign-up-container">
-                        <form action="">
-                            <h1>Create an Account</h1>
-                            <input type="text" placeholder="username" value={this.state.username}/>
-                            <input type="email" placeholder="email" value={this.state.email}/>
-                            <input type="password" placeholder="password" value={this.state.password}/>
-                            <button>Sign Up</button>
-                            <button><Link to='/'>Cancel</Link></button>
+                <div className= {loginContainer}>
+
+                    <div className="form-container sign-up-container">
+                        <form action="" className="loginForm">
+                            <h1 className="h1">Create an Account</h1>
+                            <input 
+                                className="loginInput" 
+                                type="text" 
+                                placeholder="username" 
+                                value={this.state.username}/>
+                            <input 
+                                className="loginInput" 
+                                type="email" 
+                                placeholder="email" 
+                                value={this.state.email}/>
+                            <input 
+                                className="loginInput" 
+                                type="password"
+                                placeholder="password" 
+                                value={this.state.password}/>
+                            <button className="loginBtn">Sign Up</button>
+                            
+                        </form>
+                    </div>   
+                    <div className="form-container log-in-container">
+                        <form action="" className="loginForm">
+                            <h1 className="h1">Log In</h1>
+                            <input 
+                                className="loginInput" 
+                                type="email" 
+                                placeholder="email" 
+                                value={this.state.email}/>
+                            <input 
+                                className="loginInput" 
+                                type="password" 
+                                placeholder="password" 
+                                value={this.state.password}/>
+                            <button className="loginBtn">Log In</button>
+        
                         </form>
                     </div>
-                    <div class="form-container log-in-container">
-                        <form action="">
-                        <form action="">
-                            <h1>Log In</h1>
-                            <input type="email" placeholder="email" value={this.state.email}/>
-                            <input type="password" placeholder="password" value={this.state.password}/>
-                            <button>Log In</button>
-                            <button><Link to='/'>Cancel</Link></button>
-                        </form>
-                        </form>
-                    </div>
-                    <div class="overlay-container">
-                        <div class="overlay">
-                            <div class="overlay-panel overlay-left">
-                                <h1>Welcome Back!</h1>
-                                <button class="ghost" id="signIn">Log In</button>
+                    <div className="overlay-container">
+                        <div className="overlay">
+                            <div className="overlay-panel overlay-left">
+                                <h1 className="h1">Welcome Back!</h1>
+                                <p className="p">Rejoin the path of night and hone your magical skills</p>
+                                <button 
+                                    className="ghost loginBtn" 
+                                    onClick={this.handleSlideEffect}>
+                                        Log In</button>
                             </div>
-                            <div class="overlay-panel overlay-right">
-                                <h1>Join the Path of Night</h1>
-                                    <p>Enter your personal details and start your magical journey</p>
-                                <button class="ghost" id="signUp">Sign Up</button>
+                            <div className="overlay-panel overlay-right">
+                                <h1 className="h1">Join the Path of Night</h1>
+                                    <p className="p">Enter your personal details and start your magical journey</p>
+                                <button 
+                                    className="ghost loginBtn" 
+                                    onClick={this.handleSlideEffect}>
+                                        Sign Up</button>
                             </div>
                         </div>
                     </div>
+
                 </div>
 
             </div>
