2cb6367650fcb80bd1156d2642fe0161cec552dd
ursulacj <ursulacedillojohnson@gmail.com>
2019-04-16 12:50:43 -0500

    committing second iteration of win logic

--- src/components/FullGameScreen/Cauldron/Cauldron.js
+++ src/components/FullGameScreen/Cauldron/Cauldron.js
@@ -24,40 +24,56 @@ class Cauldron extends Component {
         
         const stableFormula = this.props.stableFormula;
         const activeIngredients = this.props.activeIngredients;
-        
+
         // // this data value comes from handleDrop's data var
         if (!stableFormula.includes(activeIngredients[data])) {
             alert('Better luck next time!');
         }  else {
             alert('ingredient accepted!');
 
+            // push the item into itemsInCauldron array + update the state
             let itemsInCauldronCopy = this.state.itemsInCauldron;
             itemsInCauldronCopy.push(activeIngredients[data]);
             console.log(itemsInCauldronCopy);
 
-            this.setState({itemsInCauldron: itemsInCauldronCopy})
+            this.setState({itemsInCauldron: itemsInCauldronCopy}, );
 
-            // push the item into itemsInCauldron array
             // remove item from activeIngredients array by using this.props.handleIngredientDrop()
         }
 
+        
         
         // if itemsInCauldron.length === numStableNeeded, trigger win
         
     }
     
-    // getNumNeeded = () => {
-    //     const stableFormula = this.props.stableFormula;
-    //     const activeIngredients = this.props.activeIngredients;
+    getNumNeeded = () => {
+        const stableFormula = this.props.stableFormula;
+        const activeIngredients = this.props.activeIngredients;
 
-    //     const stableIngredients = activeIngredients.filter(val => stableFormula.includes(val));
-    //     return stableIngredients;
-    // }
+        // basically this function can't run because the consts aren't passing the props.
+        // BUT these consts are identical to the ones I'm using for the handleDrop function. In the handleDrop they're passed but NOT here 
+        const stableIngredients = activeIngredients.filter(val => stableFormula.includes(val));
+        return stableIngredients.length;
+    }
 /*--------------Lifecycle Methods----------------*/
 
     componentDidMount() {
+        // console.log(this.getNumNeeded());
+        console.log(this.props.stableFormula, 'stable did mount')
+        console.log(this.props.activeIngredients, 'active did mount')
+    }
+
+    componentDidUpdate() {
+        // to fix the prop passing issue I console.logged the function here and it worked 
+        // BUT it won't allow me to set state here because it loops too much and exceeds the maximum depth. 
+        // I think I need to pass this but ONLY once
+
         // this.setState({numStableNeeded: this.getNumNeeded()});
     }
+
+
+
 
 /*--------------Render Function----------------*/
 
